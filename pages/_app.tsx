0ce10bddd9c0a26ede0fed9d56960260e5ddc220
Paul Leones Zambrano <wp.leonesz@uea.edu.ec>
2026-06-20 08:40:54 -0500

    Refactor code structure for improved readability and maintainability

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import type { AppProps } from "next/app";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} min-h-full flex flex-col antialiased`}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
+}
